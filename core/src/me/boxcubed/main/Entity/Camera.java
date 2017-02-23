f68f85162732f30a2856f7fc425479a01dc9bdfd
TejSidhu <singhsidhu2000@gmail.com>
2017-02-23 17:42:50 +1100

    camera

---
+++ core/src/me/boxcubed/main/Entity/Camera.java
@@ -0,0 +1,33 @@
+package me.boxcubed.main.Entity;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import javax.print.StreamPrintService;
+
+/**
+ * Created by Tej Sidhu on 23/02/2017.
+ */
+public class Camera {
+    private static OrthographicCamera cam;
+    Vector2 camCoords;
+    FitViewport viewport;
+    public Camera(){
+        camCoords = new Vector2();
+        cam = new OrthographicCamera();
+        viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());//Cbs working out the ratio of the width:height
+    }
+    public Vector2 cameraXY(){
+        //This returns the co-ordinates of the camera
+        return camCoords;
+    }
+    public void create(SpriteBatch batch){
+        batch.setProjectionMatrix(cam.combined);
+    }
+    public static void update(){
+        cam.update();
+    }
+}
--- core/src/me/boxcubed/main/States/GameState.java
+++ core/src/me/boxcubed/main/States/GameState.java
@@ -1,7 +1,12 @@
 package me.boxcubed.main.States;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import me.boxcubed.main.Entity.Camera;
 
 public class GameState extends State implements Screen {
 	public GameState (GameStateManager gsm){
@@ -11,12 +16,23 @@ public class GameState extends State implements Screen {
 
 	@Override
 	protected void handleInput() {
-
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            //Player.goUp
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            //Player.goDown
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            //Player.goLeft
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            //Player.goRight
+        }
 	}
 
 	@Override
 	public void update(float delta) {
-
+        Camera.update();
 	}
 
 	@Override
@@ -36,8 +52,8 @@ public class GameState extends State implements Screen {
 
 	@Override
 	public void resize(int width, int height) {
-
-	}
+	    Camera.update();
+    }
 
 	@Override
 	public void pause() {
