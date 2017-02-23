d1301dac693e5be1a4e6dd3fe7696479e1ba3931
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-02-24 07:29:54 +1100

    Working on Player

--- core/src/me/boxcubed/main/Sprites/Player.java
+++ core/src/me/boxcubed/main/Sprites/Player.java
@@ -1,7 +1,32 @@
 package me.boxcubed.main.Sprites;
 
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
+
 /**
  * Created by Tej Sidhu on 23/02/2017.
  */
-public class Player {
+public class Player extends Sprite{
+	World w;
+	Body b;
+	
+	public Player(World w) {
+		this.w=w;
+		BodyDef bd=new BodyDef();
+		b=w.createBody(bd);
+	
+	}
+	public void render(SpriteBatch sb){
+		
+		
+	}
+	public void handleInput(Input i){
+		
+		
+		
+	}
 }
