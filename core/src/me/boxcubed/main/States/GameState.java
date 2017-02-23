73bd1b7d6139dd296f88e121279e20078314c331
TejSidhu <singhsidhu2000@gmail.com>
2017-02-23 14:19:16 +1100

    Fixes n more

---
+++ core/src/me/boxcubed/main/States/GameState.java
@@ -0,0 +1,8 @@
+public class GameState extends State implements Screen {
+	public GameState (GameState gsm){
+		//Basically the create method
+	}
+	public render(SpriteBatch batch){
+
+	}
+}
--- core/src/me/boxcubed/main/TopDown.java
+++ core/src/me/boxcubed/main/TopDown.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class TopDown extends Screen {
+public class TopDown extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	GameStateManager gsm;
@@ -22,7 +22,9 @@ public class TopDown extends Screen {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
 		batch.begin();
+		gsm.render(batch);
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
