ee006e4aa0d04cd1a7e0ce7779d741a4175ede41
TejSidhu <singhsidhu2000@gmail.com>
2017-02-27 09:19:43 +1100

    entity interface

--- core/src/me/boxcubed/main/Objects/Entity.java
+++ core/src/me/boxcubed/main/Objects/Entity.java
@@ -1,5 +1,6 @@
 package me.boxcubed.main.Objects;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -12,4 +13,6 @@ public interface Entity {
     public void goDown();
     public void goLeft();
     public void goRight();
+    public Animation animation();
+    public Sprite sprite();
 }
