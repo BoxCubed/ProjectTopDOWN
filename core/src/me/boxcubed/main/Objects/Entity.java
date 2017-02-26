2aec03f8678072a77fd8c0575086c3bd103c2176
DankMemes <heg0001@suzannecoryhs.vic.edu.au>
2017-02-27 09:33:24 +1100

    4sa-branch:
    fix

--- core/src/me/boxcubed/main/Objects/Entity.java
+++ core/src/me/boxcubed/main/Objects/Entity.java
@@ -3,16 +3,20 @@ package me.boxcubed.main.Objects;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.World;
 
 public interface Entity {
-    public Vector2 pos();
-    public void initPlayer();
-    public void goUP();
-    public void goDown();
-    public void goLeft();
-    public void goRight();
-    public Animation animation();
-    public Sprite sprite();
+	  public Vector2 getPos();
+
+	    public void setPos(Vector2 pos);
+		public void setSprite(Sprite sprite);
+		public Sprite getSprite();
+
+	    public void goUP(int vel);
+	    public void goDown(int vel);
+	    public void goLeft(int vel);
+	    public void goRight(int vel);
+	    //public Animation animation();
+	    public Sprite sprite();
+
 }
+  
