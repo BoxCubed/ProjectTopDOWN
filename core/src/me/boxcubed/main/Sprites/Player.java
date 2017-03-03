c5c220f7aa9af22a250c3f28e4dd24f1ba002fd8
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-03-03 14:34:39 +1100

    Made the Sprite display
    Doesn't bahaved as expected however...

--- core/src/me/boxcubed/main/Sprites/Player.java
+++ core/src/me/boxcubed/main/Sprites/Player.java
@@ -20,54 +20,51 @@ import me.boxcubed.main.States.GameState;
 /**
  * Created by Dank Memes on 23/02/2017.
  */
-public class Player extends Sprite implements LivingEntity
-{
+public class Player extends Sprite implements LivingEntity {
 	private static Texture tex = new Texture(Gdx.files.internal("assets/img/player.png"));
 	public BodyDef playerDef;
 	FixtureDef fixtureDefPlayer;
 	PolygonShape playerShape;
 	public Body playerBody;
 	Fixture fixture;
-	Vector2 position;//Player position
+	Vector2 position;// Player position
+
 	public Player(World world) {
-		
+
 		super(tex);
 		playerDef = new BodyDef();
 		playerDef.type = BodyDef.BodyType.DynamicBody;
-		playerDef.position.set(300/GameState.PPM, 400/GameState.PPM);
-		
-		
-		
-		
-		
-		//Shape
+		playerDef.position.set(300 / GameState.PPM, 400 / GameState.PPM);
+
+		// Shape
 		playerShape = new PolygonShape();
 		playerShape.setAsBox(5, 5);
-		//Fixture def
+		// Fixture def
 		fixtureDefPlayer = new FixtureDef();
 		fixtureDefPlayer.shape = playerShape;
-		
+
 		fixtureDefPlayer.friction = 0f;
-		//Creates the body and assigns vars to all important values
-	playerBody =world.createBody(playerDef);
-	fixture=playerBody.createFixture(fixtureDefPlayer);
-	
+		// Creates the body and assigns vars to all important values
+		playerBody = world.createBody(playerDef);
+		fixture = playerBody.createFixture(fixtureDefPlayer);
+
 	}
-	public void render(SpriteBatch sb){
-		//we do
+
+	public void render(SpriteBatch sb) {
+		// we do
 	}
+
 	@Override
 	public Vector2 getPos() {
-	    position.x = playerDef.position.x;
-	    position.y = playerDef.position.y;
-	    return position;
+		position.x = playerDef.position.x;
+		position.y = playerDef.position.y;
+		return position;
 	}
 
 	@Override
 	public void setPos(Vector2 pos) {
 
 	}
-
 
 	@Override
 	public Sprite getSprite() {
@@ -83,29 +80,31 @@ public class Player extends Sprite implements LivingEntity
 	public Sprite sprite() {
 		return null;
 	}
-	//Walking
+
+	// Walking
 	@Override
 	public void goUP() {
-		playerBody.applyLinearImpulse(new Vector2(0, 80),playerBody.getWorldCenter(), true);
+		playerBody.applyLinearImpulse(new Vector2(0, 80), playerBody.getWorldCenter(), true);
 
 	}
 
 	@Override
 	public void goDOWN() {
-		playerBody.applyLinearImpulse(new Vector2(0f, -80f),playerBody.getWorldCenter(), true);
-		}
-
+		playerBody.applyLinearImpulse(new Vector2(0f, -80f), playerBody.getWorldCenter(), true);
+	}
 
 	@Override
 	public void goLEFT() {
-		playerBody.applyLinearImpulse(new Vector2(-80f, 0),playerBody.getWorldCenter(), true);
+		playerBody.applyLinearImpulse(new Vector2(-80f, 0), playerBody.getWorldCenter(), true);
 
 	}
+
 	@Override
 	public void goRIGHT() {
-		playerBody.applyLinearImpulse(new Vector2(80, 0),playerBody.getWorldCenter(), true);
-	}//d
-	//Running actions
+		playerBody.applyLinearImpulse(new Vector2(80, 0), playerBody.getWorldCenter(), true);
+	}// d
+		// Running actions
+
 	@Override
 	public void runUP() {
 
@@ -125,7 +124,8 @@ public class Player extends Sprite implements LivingEntity
 	public void runRIGHT() {
 
 	}
-	public void stop(){
+
+	public void stop() {
 		playerBody.setLinearVelocity(0f, 0f);
 		playerBody.setAngularVelocity(0);
 	}
--- core/src/me/boxcubed/main/States/GameState.java
+++ core/src/me/boxcubed/main/States/GameState.java
@@ -20,83 +20,81 @@ import me.boxcubed.main.TopDown;
 import me.boxcubed.main.Objects.LivingEntity;
 import me.boxcubed.main.Sprites.Player;
 
+public class GameState implements Screen, InputProcessor {
+	public World gameWORLD;
+	Box2DDebugRenderer b2dr;
+	Camera cam;
+	Player player;
+	List<LivingEntity> entities;
+	SpriteBatch sb;
+	public static final int PPM = 200;
 
-public class GameState implements Screen,InputProcessor {
-    public World gameWORLD;
-    Box2DDebugRenderer b2dr;
-    Camera cam;
-    Player player;
-    List<LivingEntity>entities;
-    SpriteBatch sb;
-    public static final int PPM = 200;
-    
-	public GameState(){
-		
+	public GameState() {
+
 	}
-    
+
 	public void update(float delta) {
 		handleInput();
 		cam.update();
-        gameWORLD.step(Gdx.graphics.getDeltaTime(), 8, 2);
-       
+		gameWORLD.step(Gdx.graphics.getDeltaTime(), 8, 2);
+		player.setPosition(player.playerBody.getPosition().x, player.playerBody.getPosition().y);
 	}
 
 	@Override
 	public void render(float delta) {
 		update(delta);
 		sb.begin();
-		
-		player.setX(Gdx.graphics.getWidth()/2);
-		player.setY(Gdx.graphics.getHeight()/2);
-		
-		player.draw(sb);
-		 b2dr.render(gameWORLD,cam.combined);//Some matrix int he second argument
-		sb.end();
-       
-    }
 
-	
-	public void handleInput() {
-		//Walk controls
-		Input input=Gdx.input;
-	//And that, is how you actually do this without making a mess. Now which autistic kid decided to name the methods? cbs fixing for now
-		//boolean shiftPressed=input.isKeyPressed(Input.Keys.SHIFT_LEFT);
 		
-		if(input.isKeyPressed(Input.Keys.UP)){
+		
+		sb.draw(player, player.getX(),player.getY());
+		//b2dr.render(gameWORLD, cam.combined);
+		// Some matrix int he second argument
+		sb.end();
+
+	}
+
+	public void handleInput() {
+		// Walk controls
+		Input input = Gdx.input;
+		// And that, is how you actually do this without making a mess. Now
+		// which autistic kid decided to name the methods? cbs fixing for now
+		// boolean shiftPressed=input.isKeyPressed(Input.Keys.SHIFT_LEFT);
+
+		if (input.isKeyPressed(Input.Keys.UP)) {
 			processMovment("UP");
-		}//
-		if(input.isKeyPressed(Input.Keys.DOWN)){
+		} //
+		if (input.isKeyPressed(Input.Keys.DOWN)) {
 			processMovment("DOWN");
 		}
-		if(input.isKeyPressed(Input.Keys.LEFT)){
+		if (input.isKeyPressed(Input.Keys.LEFT)) {
 			processMovment("LEFT");
 		}
-		if(input.isKeyPressed(Input.Keys.RIGHT)){
+		if (input.isKeyPressed(Input.Keys.RIGHT)) {
 			processMovment("RIGHT");
 
 		}
 
 	}
+
 	@Override
 	public void show() {
-		
+
 		System.out.println("Init");
 		sb = new SpriteBatch();
-		entities=new ArrayList<LivingEntity>();
-        cam = new OrthographicCamera(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2);
-        gameWORLD = new World(new Vector2(0, 0), true);
-        b2dr = new Box2DDebugRenderer();
-        player = new Player(gameWORLD);
-        Gdx.input.setInputProcessor(this);
-       
-	}
+		entities = new ArrayList<LivingEntity>();
+		cam = new OrthographicCamera(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		gameWORLD = new World(new Vector2(0, 0), true);
+		b2dr = new Box2DDebugRenderer();
+		player = new Player(gameWORLD);
+		Gdx.input.setInputProcessor(this);
 
-	
+	}
 
 	@Override
 	public void resize(int width, int height) {
-        cam.update();
-    }
+		cam.update();
+	}
 
 	@Override
 	public void pause() {
@@ -117,44 +115,48 @@ public class GameState implements Screen,InputProcessor {
 	public void dispose() {
 		gameWORLD.dispose();
 		b2dr.dispose();
-		
-		player=null;
+
+		player = null;
 		entities.clear();
-		entities=null;
+		entities = null;
 		sb.dispose();
 	}
-	private boolean processMovment(String key){
+
+	private boolean processMovment(String key) {
 		String method;
-		if(Gdx.input.isKeyJustPressed(Keys.SHIFT_LEFT))method="run";
-		else method="go";
-		
-		method+=key;
+		if (Gdx.input.isKeyJustPressed(Keys.SHIFT_LEFT))
+			method = "run";
+		else
+			method = "go";
+
+		method += key;
 		Method m;
 		try {
-			//this, my friends, is reflection. Learn it. Its good.
-			m=player.getClass().getMethod(method, null);
+			// this, my friends, is reflection. Learn it. Its good.
+			m = player.getClass().getMethod(method, null);
 			m.invoke(player, null);
 			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
-		
-		
-	
+
 	}
-//BEGIN INPUT DETECTION*****************************************************
-	//***************************************************************
+
+	// BEGIN INPUT
+	// DETECTION*****************************************************
+	// ***************************************************************
 	@Override
 	public boolean keyDown(int keycode) {
 		// TODO Auto-generated method stub
-		
-		
-		switch(keycode){
+
+		switch (keycode) {
 		case Keys.ESCAPE:
-		TopDown.instance.setScreen(new GameState());break;
-		default: return false;
-		
+			TopDown.instance.setScreen(new GameState());
+			break;
+		default:
+			return false;
+
 		}
 		return true;
 	}
@@ -162,9 +164,10 @@ public class GameState implements Screen,InputProcessor {
 	@Override
 	public boolean keyUp(int key) {
 		// TODO Auto-generated method stub
-		if((key==Keys.UP||key==Keys.DOWN||key==Keys.LEFT||key==Keys.RIGHT)
-				&&(!Gdx.input.isKeyPressed(Keys.UP)&&!Gdx.input.isKeyPressed(Keys.DOWN)&&!Gdx.input.isKeyPressed(Keys.LEFT)&&!Gdx.input.isKeyPressed(Keys.RIGHT)))
-		player.stop();
+		if ((key == Keys.UP || key == Keys.DOWN || key == Keys.LEFT || key == Keys.RIGHT)
+				&& (!Gdx.input.isKeyPressed(Keys.UP) && !Gdx.input.isKeyPressed(Keys.DOWN)
+						&& !Gdx.input.isKeyPressed(Keys.LEFT) && !Gdx.input.isKeyPressed(Keys.RIGHT)))
+			player.stop();
 		return true;
 	}
 
@@ -203,8 +206,5 @@ public class GameState implements Screen,InputProcessor {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-	
-
 
 }
