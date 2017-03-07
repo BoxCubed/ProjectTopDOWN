44124da4030efb6a73ffdaf72f3fa47f39a8b60b
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-03-07 23:11:50 +1100

    Gave the spawner the ability to spawn players for no reason

--- core/src/me/boxcubed/main/Objects/Spawner.java
+++ core/src/me/boxcubed/main/Objects/Spawner.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import com.badlogic.gdx.math.Vector2;
 
+import me.boxcubed.main.Sprites.Player;
 import me.boxcubed.main.Sprites.Zombie;
 import me.boxcubed.main.States.GameState;
 
@@ -34,12 +35,20 @@ public class Spawner {
 	public void update(float delta){
 		elapsedTime+=delta;
 		if(elapsedTime>=delay){
-			if(entity.equals(EntityType.ZOMBIE)){
-				loc.x=random.nextInt(680-22)+22;
-				loc.y=random.nextInt(345-40)+40;
-				Zombie spawn=new Zombie(GameState.instance.getWorld(),  GameState.instance.playerAI);
-				spawn.getBody().setTransform(loc, spawn.getBody().getAngle());
-			GameState.instance.entities.add(spawn);
+			loc.x=random.nextInt(680-22)+22;
+			loc.y=random.nextInt(345-40)+40;
+			LivingEntity spawnEntity=null;
+			if(entity.equals(EntityType.ZOMBIE))
+				
+				spawnEntity=new Zombie(GameState.instance.getWorld(),  GameState.instance.playerAI);
+				
+			
+			if(entity.equals(EntityType.PLAYER))
+				spawnEntity=new Player(GameState.instance.getWorld());
+			
+			if(spawnEntity!=null){
+			spawnEntity.getBody().setTransform(loc, spawnEntity.getBody().getAngle());
+			GameState.instance.entities.add(spawnEntity);
 			}
 			elapsedTime=0;
 		}
