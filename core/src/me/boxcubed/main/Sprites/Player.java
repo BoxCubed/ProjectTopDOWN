18e8160cf4d2296a0fc98bafa3a761ec43e0aef6
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-03-07 22:59:26 +1100

    Player now animates

--- core/src/me/boxcubed/main/Sprites/Player.java
+++ core/src/me/boxcubed/main/Sprites/Player.java
@@ -10,6 +10,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -19,12 +21,13 @@ import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 import me.boxcubed.main.Objects.LivingEntity;
+import me.boxcubed.main.Objects.Movable;
 import me.boxcubed.main.States.GameState;
 
 /**
  * Created by Dank Memes on 23/02/2017.
  */
-public class Player extends Sprite implements LivingEntity {
+public class Player extends Sprite implements LivingEntity,Movable {
 	private float delta;
 	private static Texture tex = new Texture(Gdx.files.internal("assets/img/player.png"));
 	public BodyDef playerDef;
@@ -35,9 +38,13 @@ public class Player extends Sprite implements LivingEntity {
 	Vector2 position;// Player position
 	PlayerLight playerLight;
 	Body body;
+	private Animation<TextureRegion> animation;
+	private TextureAtlas atlas;
 	public float rotation=0;
 	public Player(World world) {
 		super(tex);
+		atlas=new TextureAtlas(Gdx.files.internal("assets/spritesheets/playersheet.atlas"));
+		animation = new Animation<TextureRegion>(1f/30f*100,atlas.getRegions());
 		playerDef = new BodyDef();
 		playerDef.type = BodyDef.BodyType.DynamicBody;
 
@@ -69,13 +76,19 @@ public class Player extends Sprite implements LivingEntity {
         playerShape.dispose();
         
 	}
+	float elapsedTime=0;
 	@Override
 	public void update(float delta) {	if(delta<1f)this.delta=1f; else this.delta=delta; 
 	handleInput();
+	elapsedTime+=delta;
 	}
 
 	public void render(SpriteBatch sb) {
+		if(playerBody.getLinearVelocity().isZero())
 		sb.draw(this, playerBody.getPosition().x-getWidth()-2/2,playerBody.getPosition().y-getHeight()/2,15,15,30,30,1,1,rotation);
+		else sb.draw(animation.getKeyFrame(elapsedTime, true), playerBody.getPosition().x-getWidth()-2/2,playerBody.getPosition().y-getHeight()/2,15,15,30,30,1,1,rotation);
+		
+			
 	}
 	public void handleInput() {
 		// Walk controls
@@ -147,8 +160,8 @@ public class Player extends Sprite implements LivingEntity {
 	}
 
 	@Override
-	public Animation animation() {
-		return null;
+	public Animation<TextureRegion> animation() {
+		return animation;
 	}
 
 	
