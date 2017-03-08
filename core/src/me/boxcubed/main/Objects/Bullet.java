444f843b6629b612d1c3a4eda39a319fcc59e7ae
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-03-08 12:29:51 +1100

    Started Bullet.
    You better catch up Tej

---
+++ core/src/me/boxcubed/main/Objects/Bullet.java
@@ -0,0 +1,16 @@
+package me.boxcubed.main.Objects;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * 
+ * @author ryan9
+ *
+ */
+public class Bullet {
+	public Bullet(Vector2 pos){
+		
+		
+	}
+
+}
