1f5fa3b40fb74d61108b7d4e4ed45fb5df38f751
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-03-08 12:45:47 +1100

    Added Entity Interface to bullet
    so noone forgets to maintain compatibility with the entity list

--- core/src/me/boxcubed/main/Objects/Bullet.java
+++ core/src/me/boxcubed/main/Objects/Bullet.java
@@ -1,15 +1,67 @@
 package me.boxcubed.main.Objects;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 
 /**
  * 
  * @author ryan9
  *
  */
-public class Bullet {
+public class Bullet implements Entity{
 	public Bullet(Vector2 pos){
 		
+		
+	}
+
+	@Override
+	public Vector2 getPos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Body getBody() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPos(Vector2 pos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Sprite getSprite() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Animation animation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(SpriteBatch sb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
 		
 	}
 
