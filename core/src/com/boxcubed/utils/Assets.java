917975bf3a86caa3c39a6cc5fb70f7751f0afe83
DankMemes <heg0001@suzannecoryhs.vic.edu.au>
2017-05-11 08:55:54 +1000

    getting rid  of unused assets

--- core/src/com/boxcubed/utils/Assets.java
+++ core/src/com/boxcubed/utils/Assets.java
@@ -20,7 +20,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.boxcubed.utils.AnimationLoader.AnimationPrefs;
 
 public class Assets extends com.badlogic.gdx.assets.AssetManager{
-	public static String nineMMGunSOUND="sounds/9_mm_gunshot.mp3",gunSOUND="sounds/gunshot.mp3",ZAttackSOUND="sounds/zombie_attack.mp3",
+	public static final String nineMMGunSOUND="sounds/9_mm_gunshot.mp3",gunSOUND="sounds/gunshot.mp3",ZAttackSOUND="sounds/zombie_attack.mp3",
 			ZGroanSOUND="sounds/zombie_groan.wav", ambientMUSIC="sounds/ambient_music.mp3",ZScreamsSOUND="sounds/zombie_screams.mp3",
 			
 			MainMAP="maps/map2.tmx",
@@ -31,6 +31,7 @@ public class Assets extends com.badlogic.gdx.assets.AssetManager{
 			
 			playerATLAS="spritesheets/playersheet.atlas",legATLAS="spritesheets/leganim.atlas",zombieAttackATLAS="spritesheets/zombieanim.atlas",
 			zombieWalkATLAS="spritesheets/zombie_walk.atlas",startATLAS="button/start.atlas",
+			rifleWALK="spritesheets/rifle_walk.atlas",
 			
 			menuFONT="fonts/menuFont.ttf",
 			
@@ -120,6 +121,7 @@ public class Assets extends com.badlogic.gdx.assets.AssetManager{
 		load(legATLAS, TextureAtlas.class);
 		load(zombieWalkATLAS, TextureAtlas.class);
 		load(startATLAS, TextureAtlas.class);
+		load(rifleWALK, TextureAtlas.class);
 	}
 	
 	@SuppressWarnings("rawtypes")
@@ -132,7 +134,7 @@ public class Assets extends com.badlogic.gdx.assets.AssetManager{
 		AnimationPrefs prefs=new AnimationPrefs();
 		prefs.duration=1/30f;
 		load(new AssetDescriptor<Animation>(zombieWalkATLAS+":anim", Animation.class,prefs));
-		
+		load(nenw AssetDescriptor)
 		
 
 	}
