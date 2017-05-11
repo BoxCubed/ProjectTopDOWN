80df544d0747dd0fe99c9bd4bdf505be780e27e7
TejSidhu <singhsidhu2000@gmail.com>
2017-05-11 10:48:30 +1000

    Inventory Sys update

--- core/src/com/boxcubed/utils/InventorySystem.java
+++ core/src/com/boxcubed/utils/InventorySystem.java
@@ -1,23 +1,49 @@
 package com.boxcubed.utils;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import me.boxcubed.main.States.GameState;
+
 public class InventorySystem {
-	InventoryItem[] inventory;
-	private static final int MAX_INVENTORY_ITEMS = 6;//Follow Java Naming convention!
+	//InventoryItem[] inventory;
+	HashMap<String, InventoryItem> inventoryItems= new HashMap<String, InventoryItem>();
+	private static final int MAX_INVENTORY_ITEMS = 6;//Follow Java Naming convention!no.
 	private int currentInventoryItems = 0; 
 	public InventorySystem(){
 		clearInventory();
 	}
-	public void addItem(int index, InventoryItem item){
+	public void addItem(String name, int index, InventoryItem item){
 		if(currentInventoryItems < MAX_INVENTORY_ITEMS){
-		inventory[index]=item;
+		//inventory[index]=item;
+		inventoryItems.put(name, item);
 		currentInventoryItems++;
+		System.out.println(name+ " has been added to the inventory");
 		}else throw new IllegalArgumentException("Given index is larger than inventory size!");
 		
 	}
 	//TODO Debugging ONLY
 	public void listItems(){
+		for(Entry<String, InventoryItem> key: inventoryItems.entrySet()){
+			System.out.println(key);
+		}
 		System.out.println("Inventory size: "+currentInventoryItems);
-		System.out.println(inventory[0]);
+		System.out.println(inventoryItems.entrySet());
+		//System.out.println(inventory[0]);
+	}
+	public void render(ShapeRenderer shaper){
+		for(Entry<String, InventoryItem> key: inventoryItems.entrySet()){
+			//System.out.println("key: "+key); This works
+		}
+	}
+	public void render(SpriteBatch batcher){
+		for(Entry<String, InventoryItem> key: inventoryItems.entrySet()){
+			//System.out.println("key: "+key); This works
+			//batcher.draw(key.getValue().getTexture(), GameState.instance.);
+		}
 	}
 	/**
 	 * May return null if no item is present in that position. 
@@ -27,18 +53,20 @@ public class InventorySystem {
 	 * @return null or item given
 	 * @throws IllegalArgumentException
 	 */
-	public InventoryItem getItem(int index){
-		return inventory[index];
+	public InventoryItem getItem(String name, int index){
+		return inventoryItems.get(name);//inventory[index];
 	}
 	
-	public void removeItem(int index){
-		System.out.println(inventory[index].getItemName()+ " has been removed from inventory");
-		inventory[index]=null;
-		
+	public void removeItem(String name, int index){
+		/*System.out.println(inventory[index].getItemName()+ " has been removed from inventory");
+		inventory[index]=null;*/
+		inventoryItems.remove(name);
 		currentInventoryItems--;
 	}
 	public void clearInventory(){
-		inventory=new InventoryItem[MAX_INVENTORY_ITEMS];
+		inventoryItems.clear();
+		//inventory=new InventoryItem[MAX_INVENTORY_ITEMS];
+		//See in hashmaps how there is a method to clear the array, it would be great of we could go back to it
 	}
 	
 }
--- core/src/me/boxcubed/main/Sprites/Player.java
+++ core/src/me/boxcubed/main/Sprites/Player.java
@@ -122,7 +122,7 @@ public class Player implements LivingEntity, Movable {
 		inventory = new InventorySystem();
 		//adding items here, for now
 		//will create a method later to add items on events eg. walking over gun or heath pack
-		inventory.addItem(1, ak);
+		inventory.addItem("ak47", 1, ak);
 		inventory.listItems();
 
 		gunshotSound = TopDown.assets.get(Assets.gunSOUND, Sound.class);
