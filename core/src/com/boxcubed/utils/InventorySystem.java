e3b0e7fa482631148d70cad33914c3dce1f63db6
TejSidhu <singhsidhu2000@gmail.com>
2017-05-16 12:14:56 +1000

    Inv items can be selected and changed

--- core/src/com/boxcubed/utils/InventorySystem.java
+++ core/src/com/boxcubed/utils/InventorySystem.java
@@ -1,17 +1,22 @@
 package com.boxcubed.utils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector3;
 
 import me.boxcubed.main.States.GameState;
 
 public class InventorySystem {
 	//InventoryItem[] inventory;
-	HashMap<String, InventoryItem> inventoryItems= new HashMap<String, InventoryItem>();
+	LinkedHashMap<Integer, InventoryItem> inventoryItems= new LinkedHashMap<Integer, InventoryItem>();
 	private static final int MAX_INVENTORY_ITEMS = 6;//Follow Java Naming convention!no.
 	private int currentInventoryItems = 0; 
 	private static final int OFFSET = 80;
@@ -22,7 +27,7 @@ public class InventorySystem {
 	public void addItem(String name, int index, InventoryItem item){
 		if(currentInventoryItems < MAX_INVENTORY_ITEMS){
 		//inventory[index]=item;
-		inventoryItems.put(name, item);
+		inventoryItems.put(index, item);
 		currentInventoryItems++;
 		System.out.println(name+ " has been added to the inventory");
 		}else throw new IllegalArgumentException("Given index is larger than inventory size!");
@@ -30,7 +35,7 @@ public class InventorySystem {
 	}
 	//TODO Debugging ONLY
 	public void listItems(){
-		for(Entry<String, InventoryItem> key: inventoryItems.entrySet()){
+		for(Entry<Integer, InventoryItem> key: inventoryItems.entrySet()){
 			System.out.println(key);
 		}
 		System.out.println("Inventory size: "+currentInventoryItems);
@@ -38,16 +43,29 @@ public class InventorySystem {
 		//System.out.println(inventory[0]);
 	}
 	float previousX;
+	Vector3 touchPos = new Vector3();
 	public void render(SpriteBatch batcher){
 		batcher.draw(inventoryUI, GameState.instance.hud.textCam.position.x/2 - GameState.instance.hud.textCam.viewportWidth/3, GameState.instance.hud.textCam.position.y/4 - GameState.instance.hud.textCam.viewportHeight/2, 650, 80);
-		for(Entry<String, InventoryItem> key: inventoryItems.entrySet()){
+		for(Entry<Integer, InventoryItem> key: inventoryItems.entrySet()){
 			//batcher.draw(key.getValue().getTexture(), GameState.instance.hud.textCam.position.x/2 - GameState.instance.hud.textCam.viewportWidth/3,GameState.instance.hud.textCam.position.y/3 - GameState.instance.hud.textCam.viewportHeight/3, 80, 80);
 			float x = GameState.instance.hud.textCam.position.x/2 - GameState.instance.hud.textCam.viewportWidth/3;//+ key.getValue().getIndex()*OFFSET;
 		    float y = GameState.instance.hud.textCam.position.y/4 - GameState.instance.hud.textCam.viewportHeight/2;
-		    batcher.draw(key.getValue().getTexture(),previousX + OFFSET*key.getValue().getIndex(),y, 80, 80);
+			if(key.getKey() == itemSelected){
+				batcher.draw(key.getValue().getTexture(),previousX + OFFSET*key.getValue().getIndex(),y, 60, 60);
+			}else{
+			    batcher.draw(key.getValue().getTexture(),previousX + OFFSET*key.getValue().getIndex(),y, 80, 80);
+			}
 		    //System.out.println();
 		    previousX = x;
-		 
+		    }
+		update(Gdx.graphics.getDeltaTime());
+	}
+	public int itemSelected = 0;
+	public void update(float delta){
+		if(Gdx.input.isKeyPressed(Keys.NUM_1)){
+			itemSelected = 0;
+		}else if(Gdx.input.isKeyPressed(Keys.NUM_2)){
+			itemSelected = 1;
 		}
 	}
 	
@@ -58,7 +76,7 @@ public class InventorySystem {
 	public void removeItem(String name, int index){
 		/*System.out.println(inventory[index].getItemName()+ " has been removed from inventory");
 		inventory[index]=null;*/
-		inventoryItems.remove(name);
+		inventoryItems.remove(index);
 		currentInventoryItems--;
 	}
 	public void clearInventory(){
--- core/src/me/boxcubed/main/States/MenuState.java
+++ core/src/me/boxcubed/main/States/MenuState.java
@@ -33,238 +33,230 @@ import me.boxcubed.main.TopDown;
  */
 public class MenuState implements Screen {
 	private Stage stage;
-    private MenuButton clickButton;
-    private MenuButton multiplayerButton;
-    private TextField ipField,nameField;
-    private float alpha=0;
-    
-    SpriteBatch batch;
-    private ShapeRenderer fader;
-    private OrthographicCamera cam;
-    
-    BitmapFont font;
-    GameState loadedInstance;
-    GlyphLayout startGlyph,multiGlyph;
-    ParallaxBackground bg;
-    NetworkManager connection;
-    boolean clicked=false;
-    float elapsedTime=0;
-    // Thread safe startup...DONE!
-    private boolean init=false;
-    public MenuState(GameState loadedInstance) {
-       
-    	this.loadedInstance=loadedInstance;
-    }
+	private MenuButton clickButton;
+	private MenuButton multiplayerButton;
+	private TextField ipField, nameField;
+	private float alpha = 0;
+
+	SpriteBatch batch;
+	private ShapeRenderer fader;
+	private OrthographicCamera cam;
+
+	BitmapFont font;
+	GameState loadedInstance;
+	GlyphLayout startGlyph, multiGlyph;
+	ParallaxBackground bg;
+	NetworkManager connection;
+	boolean clicked = false;
+	float elapsedTime = 0;
+	// Thread safe startup...DONE!
+	private boolean init = false;
+
+	public MenuState(GameState loadedInstance) {
+
+		this.loadedInstance = loadedInstance;
+	}
 
 	private void init(GameState loadedInstance) {
-		//Init of debug shape rendrer
-		  batch=new SpriteBatch();
-		  fader=new ShapeRenderer(8);
-		  cam=new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-        //init of button
-		
-        initButton(loadedInstance);
-      
-        
-        
-        //Stage setup
-        stage=new Stage(new StretchViewport(1280,900,cam) ,batch);
-        ipField=new TextField("localhost:22222", TopDown.assets.get(Assets.menuSKIN, Skin.class));
-        nameField=new TextField("BoxCubed", TopDown.assets.get(Assets.menuSKIN, Skin.class));
-        BoxoUtil.addInputProcessor(stage);
-        //ip field
-        ipField.setPosition(Gdx.graphics.getWidth()/2-150,Gdx.graphics.getHeight()/2-150);
-        ipField.setSize(300, 30);
-        stage.addActor(ipField);
-        //name field
-        nameField.setPosition(Gdx.graphics.getWidth()/2-150,Gdx.graphics.getHeight()/2-200);
-        stage.addActor(nameField);
-        nameField.setSize(300, 30);
-        
-        //Background setup
-        TextureRegion bgRegion=new TextureRegion(TopDown.assets.get(Assets.scrollMenuIMAGE, Texture.class));
-        bg=new ParallaxBackground(new ParallaxLayer[]{
-        		new ParallaxLayer(bgRegion, new Vector2(0,100),new Vector2(), new Vector2())
-        }, (float)Gdx.graphics.getWidth()/2, (float)Gdx.graphics.getHeight()/2, new Vector2(0, 1));
-		init=true;
+		// Init of debug shape rendrer
+		batch = new SpriteBatch();
+		fader = new ShapeRenderer(8);
+		cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		// init of button
+
+		initButton(loadedInstance);
+
+		// Stage setup
+		stage = new Stage(new StretchViewport(1280, 900, cam), batch);
+		ipField = new TextField("localhost:22222", TopDown.assets.get(Assets.menuSKIN, Skin.class));
+		nameField = new TextField("BoxCubed", TopDown.assets.get(Assets.menuSKIN, Skin.class));
+		BoxoUtil.addInputProcessor(stage);
+		// ip field
+		ipField.setPosition(Gdx.graphics.getWidth() / 2 - 150, Gdx.graphics.getHeight() / 2 - 150);
+		ipField.setSize(300, 30);
+		stage.addActor(ipField);
+		// name field
+		nameField.setPosition(Gdx.graphics.getWidth() / 2 - 150, Gdx.graphics.getHeight() / 2 - 200);
+		stage.addActor(nameField);
+		nameField.setSize(300, 30);
+
+		// Background setup
+		TextureRegion bgRegion = new TextureRegion(TopDown.assets.get(Assets.scrollMenuIMAGE, Texture.class));
+		bg = new ParallaxBackground(
+				new ParallaxLayer[] { new ParallaxLayer(bgRegion, new Vector2(0, 100), new Vector2(), new Vector2()) },
+				(float) Gdx.graphics.getWidth() / 2, (float) Gdx.graphics.getHeight() / 2, new Vector2(0, 1));
+		init = true;
 	}
 
 	public void handleInput() {
 
-    }
+	}
 
-    public void update(float delta) {
-    	if(!init)init(loadedInstance);
-    	if(alpha<1f){
-    		alpha+=0.005f*delta;
-    	}
-    	cam.update();
-    	stage.act();
-    	elapsedTime+=delta;
-    	clickButton.update(delta,cam);
-    	multiplayerButton.update(delta,cam);
-    	if(elapsedTime>2 )return;
-    	clickButton.setY(900-Bounce.easeOut(elapsedTime, 0, Gdx.graphics.getHeight()/2, 2));
-    	multiplayerButton.setY(900-Bounce.easeOut(elapsedTime, 0, Gdx.graphics.getHeight()/2-100, 2));
-    	
-    	
+	public void update(float delta) {
+		if (!init)
+			init(loadedInstance);
+		if (alpha < 1f) {
+			alpha += 0.005f * delta;
+		}
+		cam.update();
+		stage.act();
+		elapsedTime += delta;
+		clickButton.update(delta, cam);
+		multiplayerButton.update(delta, cam);
+		if (elapsedTime > 2)
+			return;
+		clickButton.setY(900 - Bounce.easeOut(elapsedTime, 0, Gdx.graphics.getHeight() / 2, 2));
+		multiplayerButton.setY(900 - Bounce.easeOut(elapsedTime, 0, Gdx.graphics.getHeight() / 2 - 100, 2));
 
-    }
+	}
 
-    @Override
-    public void show() {
-    	
+	@Override
+	public void show() {
 
-    }
+	}
 
-    @Override
-    public void render(float delta) {
-       
-    	update(delta);
-    	bg.render(delta);
-    	fader.setProjectionMatrix(cam.combined);
-    	fader.begin(ShapeType.Line);
-        fader.setColor(0,0,0,alpha);
-        clickButton.render(fader, cam);
-        multiplayerButton.render(fader, cam);
-      // fader.rect(0, 0, cam.viewportWidth, cam.viewportHeight);
-        fader.end();
-        
-        batch.setProjectionMatrix(cam.combined);
-        batch.begin();
-        
-        clickButton.render(batch,cam);
-        multiplayerButton.render(batch,cam);
+	@Override
+	public void render(float delta) {
 
-        batch.end();
-        stage.draw();
-        
-        
-    }
+		update(delta);
+		bg.render(delta);
+		fader.setProjectionMatrix(cam.combined);
+		fader.begin(ShapeType.Line);
+		fader.setColor(0, 0, 0, alpha);
+		clickButton.render(fader, cam);
+		multiplayerButton.render(fader, cam);
+		// fader.rect(0, 0, cam.viewportWidth, cam.viewportHeight);
+		fader.end();
 
-    private void initButton(GameState loadedInstance){
-    	font = TopDown.assets.get(Assets.menuFONT, BitmapFont.class);
-        //setting color for chosen
-        font.setColor(Color.GREEN);
-        //making glyph fir button
-        startGlyph = new GlyphLayout();
-        startGlyph.setText(font, "Start Singleplayer");
-        //Making button with listener
-        clickButton=new MenuButton(startGlyph,font,  Gdx.graphics.getWidth()/2 - startGlyph.width/2,  Gdx.graphics.getHeight()/2, 
-        		new MenuListener() {
-			@Override
-			public void rightclicked(MenuButton m) {
-				System.out.println("RClicked!");
-				
-			}
-			@Override
-			public void notChosen(MenuButton m) {
-			
-				
-			}
-			@Override
-			public void clicked(MenuButton m) {
-				
+		batch.setProjectionMatrix(cam.combined);
+		batch.begin();
+
+		clickButton.render(batch, cam);
+		multiplayerButton.render(batch, cam);
+
+		batch.end();
+		stage.draw();
+
+	}
+
+	private void initButton(GameState loadedInstance) {
+		font = TopDown.assets.get(Assets.menuFONT, BitmapFont.class);
+		// setting color for chosen
+		font.setColor(Color.GREEN);
+		// making glyph fir button
+		startGlyph = new GlyphLayout();
+		startGlyph.setText(font, "Start Singleplayer");
+		// Making button with listener
+		clickButton = new MenuButton(startGlyph, font, Gdx.graphics.getWidth() / 2 - startGlyph.width / 2,
+				Gdx.graphics.getHeight() / 2, new MenuListener() {
+					@Override
+					public void rightclicked(MenuButton m) {
+						System.out.println("RClicked!");
+
+					}
+
+					@Override
+					public void notChosen(MenuButton m) {
+
+					}
+
+					@Override
+					public void clicked(MenuButton m) {
+
 						loadedInstance.newPlayer(0);
-				
-                TopDown.instance.setScreen(loadedInstance);
-			}
-			@Override
-			public void chosen(MenuButton m) {
-				
-			}
-		});
-        
-        
-        
-        //making glyph for when button is not selected
-        font.setColor(Color.RED);
-        //assigning it
-        clickButton.setGlyphChosen(new GlyphLayout(font, "Start Singleplayer"));
-        //locking collision so the box won't change with font
-        clickButton.setCollisionLock(true);
-        //assigning the dimensions for collision since we locked it
-        clickButton.getRect().width=clickButton.f.width;
-        clickButton.getRect().height=clickButton.f.height;
-        font.setColor(Color.GREEN);
-        multiGlyph=new GlyphLayout(font, "Start Multiplayer");
-    	multiplayerButton=new MenuButton(multiGlyph, font, Gdx.graphics.getWidth()/2-multiGlyph.width/2, Gdx.graphics.getHeight()/2-100, 
-    			new MenuListener() {
-					
+
+						TopDown.instance.setScreen(loadedInstance);
+					}
+
+					@Override
+					public void chosen(MenuButton m) {
+
+					}
+				});
+
+		// making glyph for when button is not selected
+		font.setColor(Color.RED);
+		// assigning it
+		clickButton.setGlyphChosen(new GlyphLayout(font, "Start Singleplayer"));
+		// locking collision so the box won't change with font
+		clickButton.setCollisionLock(true);
+		// assigning the dimensions for collision since we locked it
+		clickButton.getRect().width = clickButton.f.width;
+		clickButton.getRect().height = clickButton.f.height;
+		font.setColor(Color.GREEN);
+		multiGlyph = new GlyphLayout(font, "Start Multiplayer");
+		multiplayerButton = new MenuButton(multiGlyph, font, Gdx.graphics.getWidth() / 2 - multiGlyph.width / 2,
+				Gdx.graphics.getHeight() / 2 - 100, new MenuListener() {
+
 					@Override
 					public void notChosen(MenuButton m) {
 						font.setColor(Color.GREEN);
-						if(clicked&&connection!=null)
-						multiGlyph.setText(font, connection.state.toString().toLowerCase());
-						else 
+						if (clicked && connection != null)
+							multiGlyph.setText(font, connection.state.toString().toLowerCase());
+						else
 							multiGlyph.setText(font, "Start Multiplayer");
-						
+
 					}
-					
+
 					@Override
 					public void clicked(MenuButton m) {
-						clicked=true;
-						
-						if(connection==null){
-							connection=new NetworkManager(loadedInstance.player,ipField.getText());
-							connection.name=nameField.getText();
-							
-						}else if(connection.state.equals(ConnectionState.CONNECTING)){
+						clicked = true;
+
+						if (connection == null) {
+							connection = new NetworkManager(loadedInstance.player, ipField.getText());
+							connection.name = nameField.getText();
+
+						} else if (connection.state.equals(ConnectionState.CONNECTING)) {
 							return;
-							}
-						else if(connection.state.equals(ConnectionState.CONNECTED)){
+						} else if (connection.state.equals(ConnectionState.CONNECTED)) {
 							loadedInstance.player.setConnection(connection, 1);
-							TopDown.instance.setScreen(loadedInstance);}
-							
-					
-						else if(connection.state.equals(ConnectionState.INVALIDIP)||connection.state.equals(ConnectionState.DISCONNECTED)){
-							connection=new NetworkManager(loadedInstance.player,ipField.getText());
+							TopDown.instance.setScreen(loadedInstance);
 						}
-						
+
+				else if (connection.state.equals(ConnectionState.INVALIDIP) || connection.state.equals(ConnectionState.DISCONNECTED)) {
+							connection = new NetworkManager(loadedInstance.player, ipField.getText());
+						}
+
 					}
-					
+
 					@Override
 					public void chosen(MenuButton m) {
 						font.setColor(Color.RED);
-						if(clicked&&connection!=null)
-						multiGlyph.setText(font, connection.state.toString().toLowerCase());
-						else 
+						if (clicked && connection != null)
+							multiGlyph.setText(font, connection.state.toString().toLowerCase());
+						else
 							multiGlyph.setText(font, "Start Multiplayer");
-							
-						
+
 					}
 				});
-    	
-    }
 
+	}
 
-    @Override
-    public void resize(int width, int height) {
-    }
+	@Override
+	public void resize(int width, int height) {
+	}
 
-    @Override
-    public void pause() {
+	@Override
+	public void pause() {
 
-    }
+	}
 
-    @Override
-    public void resume() {
+	@Override
+	public void resume() {
 
-    }
+	}
 
-    @Override
-    public void hide() {
-    	if(init)
-    	BoxoUtil.remInputProcessor(stage);
-    }
+	@Override
+	public void hide() {
+		if (init)
+			BoxoUtil.remInputProcessor(stage);
+	}
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        stage.dispose();
-        stage.getBatch().dispose();
-    }
-  
-  
-   
+	@Override
+	public void dispose() {
+		batch.dispose();
+		stage.dispose();
+		stage.getBatch().dispose();
+	}
+
 }
