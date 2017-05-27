2d6169249bd9976bca9de9a93c60ae0c263d29cd
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-05-27 14:45:16 +1000

    Made the app run fullscreen

--- android/src/me/boxcubed/main/AndroidLauncher.java
+++ android/src/me/boxcubed/main/AndroidLauncher.java
@@ -1,6 +1,7 @@
 package me.boxcubed.main;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -12,5 +13,20 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new TopDown(false), config);
+	}
+
+	@Override
+	public  void onWindowFocusChanged(boolean  hasFocus){
+		super.onWindowFocusChanged(hasFocus);
+		View decorView = getWindow().getDecorView();
+		if(hasFocus){
+
+			decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					|View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY  // this flag do=Semi-transparent bars temporarily appear and then hide again
+					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN  // Make Content Appear Behind the status  Bar
+					|View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION  // it Make Content Appear Behind the Navigation Bar
+					|View.SYSTEM_UI_FLAG_FULLSCREEN  // hide status bar
+					|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+		}
 	}
 }
